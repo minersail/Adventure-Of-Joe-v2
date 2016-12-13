c0ce8fdc84d7fbc6429c291f2f529d3216b29bfc
minersail <jkwoo1234@gmail.com>
2016-12-13 16:02:15 -0500

    Blank Project

---
+++ MSJGame/desktop/src/woohoo/msjgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package woohoo.msjgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import woohoo.msjgame.MSJGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MSJGame(), config);
+	}
+}
