c4b338f46e84ed4d17195ec75759a2081355a246
minersail <jkwoo1234@gmail.com>
2017-01-11 22:49:52 -0500

    v1.2.2
    Continued updated dialogues
    
    TODO:
    lots of obvious stuff
    XML text storage
    Learn scene2d.ui

--- MSJGame/core/src/woohoo/framework/DialogueManager.java
+++ MSJGame/core/src/woohoo/framework/DialogueManager.java
@@ -1,26 +1,65 @@
 package woohoo.framework;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import woohoo.gameobjects.components.DialogueComponent;
 
 public class DialogueManager
 {
     private DialogueComponent currentDialogue;
     private TextureRegion displayBox;
+	private Stage dialogueUI;
     
-    public DialogueManager(TextureRegion tr)
+    public DialogueManager(TextureRegion tr, FitViewport vp, BitmapFont font)
     {
         displayBox = tr;
+		dialogueUI = new Stage(vp);
+		
+		final Label label = new Label("Hello", new LabelStyle(font, Color.GREEN));
+		label.setWidth(1);
+		label.setHeight(1);
+		label.setPosition(6, 10);
+		label.setFontScale(0.025f);
+				
+		label.addListener(new InputListener() 
+		{
+            @Override
+            public boolean keyDown(InputEvent event, int keycode) 
+			{
+                if (keycode == Keys.SPACE)
+				{
+					label.setText("Bye");
+				}
+				
+                return true;
+            }
+        });		
+		
+		dialogueUI.addActor(label);
+		dialogueUI.setKeyboardFocus(label);
     }
     
-    public void setDialogue(DialogueComponent dia)
+    public void startDialogue(DialogueComponent dia)
     {
         currentDialogue = dia;
+		Gdx.input.setInputProcessor(dialogueUI);
     }
     
-    public void runDialogue()
+    public void runDialogue(float delta)
     {
-        
+        dialogueUI.act(delta);
+		dialogueUI.draw();
+		System.out.println(dialogueUI.getActors().get(0).getHeight());
     }
     
     public TextureRegion getBox()
